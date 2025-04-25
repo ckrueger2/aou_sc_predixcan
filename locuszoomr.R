53a2ebfc6208b355fe571fdc2d7fa4bb52b8e932
Drew Patterson <dwpatterson830@gmail.com>
2025-04-24 20:19:04 -0500

    Updated for wrapper

--- locuszoomr.R
+++ locuszoomr.R
@@ -13,11 +13,25 @@ library(EnsDb.Hsapiens.v86)
 install.packages("locuszoomr")
 library(locuszoomr)
 
+# installing argparse library
+install.packages("argparse")
+library(argparse)
+
+# Create parser object
+parser <- ArgumentParser()
+
+# specify our desired options: RSID and LD Token
+parser$add_argument("--rsid", help = "Specify an index SNP rsID to plot", default = NULL)
+parser$add_argument("--token", help = "Specify unique LD Link user token to plot with LD 
+                    information", default = NULL)
+parser$add_argument("-i", "--input", help = "Specify the name of the GWAS summary statistics file", default = NULL)
+args <- parser$parse_args()
+
 # Get the bucket name
 my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
 
 # replace empty quotes with the name of the file in your google bucket (don't delete the quotation marks)
-name_of_file_in_bucket = ""
+name_of_file_in_bucket = (args$input)
 
 # Get the bucket name
 my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
@@ -33,12 +47,29 @@ data <- read.table(file = name_of_file_in_bucket,
 colnames(data)[colnames(data) == "P"] <- "p"
 colnames(data)[colnames(data) == "CHR"] <- "chrom"
 
-# creating locus object and linking LD link
-loc2 <- locus(data = data_small, ens_db = "EnsDb.Hsapiens.v86", index_snp = "REPLACE WITH SNP ID OF INTEREST", 
-              flank = 1e5)
+# if rsid is user provided
+if (!is.null(args$rsid)) {
+  # creating locus object with user provided SNP
+  loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = args$rsid, 
+                flank = 1e5)
+} else { 
+  # rsid is not provided, so we default to most signif. SNP
+  signif_rsid <- data$rsIS[which.min(data$p)]
 
-loc2 <- link_LD(loc2, token = "REPLACE WITH LD LINK TOKEN")
+  # creating locus object with top hit SNP
+  loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = signif_rsid, 
+                flank = 1e5)
+}
 
-# Plotting locuszoomr plot
-# plotting locuszoomr
-locus_plot(loc2, labels = c("index"), label_x = c(4, -5))
+# if LD link token is provided
+if (!is.null(args$token)) {
+  # creating locus object with LD info
+  loc <- link_LD(loc, token = args$token)
+
+  # plot locus with LD info
+  locus_plot(loc, labels = c("index"), label_x = c(4, -5))
+} else {
+  # plot locus without LD info
+  locus_plot(loc, labels = c("index"), label_x = c(4, -5))
+}
+
