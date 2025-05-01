20331a1a6a292f348d53d1cbe235e8767b84ca1b
Claudia Krueger <ckrueger2@luc.edu>
2025-05-01 12:16:29 -0500

    Update 00wrapper.sh

--- 00wrapper.sh
+++ 00wrapper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 #command
 usage() {
     echo "Usage: $0 --phecode <PHECODE> --pop <POP> [--rsid <RSID>] [--token <TOKEN>]"
     exit 1
 }
+
 #command-line arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -29,21 +31,28 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
+
 #check for required arguments
 if [[ -z "$PHECODE" || -z "$POP" ]]; then
     usage
 fi
+
+#github repo path
+REPO=$HOME/GWAS-TWAS-in-All-of-Us-Cloud
+
 #download hail table
-python "/home/jupyter/GWAS-TWAS-in-All-of-US-Cloud/pull_data.py" --phecode "$PHECODE" --pop "$POP"
+python "$REPO/01pull_data.py" --phecode "$PHECODE" --pop "$POP"
+#capture the SNP count
+SNP_COUNT=$(python "$REPO/01pull_data.py" --phecode "$PHECODE" --pop "$POP" | tail -n 1)
 
 #format hail tables
-Rscript "/home/jupyter/GWAS-TWAS-in-All-of-US-Cloud/table_format.R" --phecode "$PHECODE" --pop "$POP"
+Rscript "$REPO/02table_format.R" --phecode "$PHECODE" --pop "$POP"
 
 #GWAS qqman
-python "/home/jupyter/GWAS-TWAS-in-All-of-US-Cloud/qqman.py" --phecode "$PHECODE" --pop "$POP"
+Rscript "$REPO/03qqman.R" --phecode "$PHECODE" --pop "$POP" --snp_count "$SNP_COUNT"
 
 #locuszoomR
-LOCUSZOOM_CMD="Rscript /home/jupyter/GWAS-TWAS-in-All-of-US-Cloud/locuszoom.R --phecode \"$PHECODE\" --pop \"$POP\""
+LOCUSZOOM_CMD="Rscript $REPO/04locuszoom.R --phecode \"$PHECODE\" --pop \"$POP\""
 if [[ ! -z "$RSID" ]]; then
     LOCUSZOOM_CMD="$LOCUSZOOM_CMD --rsid \"$RSID\""
 fi
@@ -52,4 +61,5 @@ if [[ ! -z "$TOKEN" ]]; then
 fi
 eval $LOCUSZOOM_CMD
 
+#how to view generated PNG files
 echo "To view the PNG files, go to the Jupyter file browser by selecting the jupyter logo to the top left of the terminal."
