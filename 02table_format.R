e99f2216e2045b9330bf18288ccb296d02a0f9bb
Claudia Krueger <ckrueger2@luc.edu>
2025-05-02 17:44:56 -0500

    Update 02table_format.R

--- 02table_format.R
+++ 02table_format.R
@@ -66,23 +66,23 @@ if (check_result != 0) {
 
 #PERFORM COMMAND LINE FORMATTING FOR S-PREDIXCAN FILE
 #upload GTEx SNP file to workspace bucket
-command7 <- paste0("gsutil -m cp -v ~/GWAS-TWAS-in-All-of-Us-Cloud/predixcan_models_varids-effallele.txt.gz ", my_bucket, "/data/")
-system(command7, intern=TRUE)
+#command7 <- paste0("gsutil -m cp -v ~/GWAS-TWAS-in-All-of-Us-Cloud/predixcan_models_varids-effallele.txt.gz ", my_bucket, "/data/")
+#system(command7, intern=TRUE)
 
 #unzip files
-command8 <- paste0("gsutil cat ", my_bucket, "/data/predixcan_models_varids-effallele.txt.gz | gunzip > /tmp/predixcan_models_varids-effallele.txt")
-system(command8)
+#command8 <- paste0("gsutil cat ", my_bucket, "/data/predixcan_models_varids-effallele.txt.gz | gunzip > /tmp/predixcan_models_varids-effallele.txt")
+#system(command8)
 
 #format reference file
-system("awk -F'[,:]' 'NR>1 {print $1\":\"$2}' /tmp/predixcan_models_varids-effallele.txt > /tmp/chrpos_allele_table.tsv", intern=TRUE)
+#system("awk -F'[,:]' 'NR>1 {print $1\":\"$2}' /tmp/predixcan_models_varids-effallele.txt > /tmp/chrpos_allele_table.tsv", intern=TRUE)
 
 #make temp files
-command9 <- paste0("gsutil cp ", my_bucket, "/data/", args$pop, "_full_", args$phecode,".tsv /tmp/")
-system(command9)
+#command9 <- paste0("gsutil cp ", my_bucket, "/data/", args$pop, "_full_", args$phecode,".tsv /tmp/")
+#system(command9)
 
 #filter SNPs
 #command10 <- paste0("awk 'NR==FNR {a[$1]; next} ($1) in a' /tmp/chrpos_allele_table.tsv /tmp/", args$pop, "_full_", args$phecode, ".tsv > /tmp/gtex_", args$phecode, ".tsv")
-awk 'NR==FNR {split($1,b,":"); key=b[1]":"b[2]; a[key]; next} {key=$1":"$2; if(key in a) print}' /tmp/chrpos_allele_table.tsv /tmp/"${args[pop]}"_full_"${args[phecode]}".tsv > /tmp/gtex_"${args[phecode]}".tsv
+command10 <- paste0(awk 'NR==FNR {split($1,b,":"); key=b[1]":"b[2]; a[key]; next} {key=$1":"$2; if(key in a) print}' /tmp/chrpos_allele_table.tsv /tmp/"${args[pop]}"_full_"${args[phecode]}".tsv > /tmp/gtex_"${args[phecode]}".tsv)
 system(command10)
 
 #save to bucket
