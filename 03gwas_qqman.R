5e5e72ab10febd9df283de3e3e841927ed23edbd
Claudia Krueger <ckrueger2@luc.edu>
2025-05-01 19:28:25 -0500

    Update 03gwas_qqman.R

--- 03gwas_qqman.R
+++ 03gwas_qqman.R
@@ -29,27 +29,40 @@ hail_table  <- fread(name_of_file_in_bucket, header=TRUE)
 #what to save file as
 destination_filename <- paste0(args$pop, "_", args$phecode,"_gwas_manhattan.png")
 
-#find bonferroni correction value
-b_value <- (0.05/args$snp_count)
-log_b_value <- -log10(b_value)
-cat("Bonferroni corrected P-value: ", b_value, "\n")
-
 #manhattan plot title name
 title <- paste0(args$pop, " ", args$phecode, " GWAS Manhattan Plot")
 
 #png destination
 png(filename = destination_filename, width = 1200, height = 800, res = 100)
 
-#create the Manhattan plot
-manhattan(hail_table,
-          main = title,
-          chr = "CHR", 
-          bp = "POS", 
-          p = "Pvalue", 
-          snp = "SNP", 
-          col = c("lightblue3", "lightblue4"),
-          suggestiveline = log_b_value)
-
+#create the Manhattan plot; only use Bonferroni line if SNP count is provided
+if (!is.null(args$snp_count)) {
+  #calc Bonferroni correction value
+  b_value <- (0.05/args$snp_count)
+  log_b_value <- -log10(b_value)
+  cat("Bonferroni corrected P-value: ", b_value, "\n")
+  
+  #manhattan plot with suggestive line
+  manhattan(hail_table,
+            main = title,
+            chr = "CHR", 
+            bp = "POS", 
+            p = "Pvalue", 
+            snp = "SNP", 
+            col = c("lightblue3", "lightblue4"),
+            suggestiveline = log_b_value)
+} else {
+  #manhattan plot without suggestive line
+  cat("SNP count not provided, plotting without Bonferroni correction line\n")
+  manhattan(hail_table,
+            main = title,
+            chr = "CHR", 
+            bp = "POS", 
+            p = "Pvalue", 
+            snp = "SNP", 
+            col = c("lightblue3", "lightblue4"),
+            suggestiveline = FALSE)
+}
 dev.off()
 
 cat("GWAS Manhattan plot complete\n")
