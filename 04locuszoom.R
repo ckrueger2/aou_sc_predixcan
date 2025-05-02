b548e79923243a12fc3d086314cea6db8c0dcf93
Claudia Krueger <ckrueger2@luc.edu>
2025-05-01 19:20:18 -0500

    Update 04locuszoom.R

--- 04locuszoom.R
+++ 04locuszoom.R
@@ -43,7 +43,7 @@ if (!is.null(args$rsid)) {
   signif_rsid <- data$rsID[which.min(data$Pvalue)]
   # creating locus object with top hit SNP
   cat("rsID not provided; running locuszoom on lowest p-value SNP\n")
-  loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = signif_rsid, flank = 1e5, snp_col = "rsID", pvalue_col = "Pvalue")
+  loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = signif_rsid, flank = 1e5)
 }
 
 #make a one-time request for your personal access token from a web browser at https://ldlink.nih.gov/?tab=apiaccess.
