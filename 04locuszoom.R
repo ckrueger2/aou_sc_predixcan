9bd2b50c64f9bef1dbdc38b293c75ae43c1da464
Claudia Krueger <ckrueger2@luc.edu>
2025-05-01 19:11:57 -0500

    Update 04locuszoom.R

--- 04locuszoom.R
+++ 04locuszoom.R
@@ -44,7 +44,7 @@ if (!is.null(args$rsid)) {
   loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = args$rsid, flank = 1e5)
 } else { 
   #rsid is not provided, so we default to most signif. SNP
-  signif_rsid <- data$rsID[which.min(data$p)]
+  signif_rsid <- data$rsID[which.min(data$P)]
   # creating locus object with top hit SNP
   cat("rsID not provided; running locuszoom on lowest p-value SNP\n")
   loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = signif_rsid, 
