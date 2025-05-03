85c387e9e0bd4973c5754fdf31adbb6215b91e1b
Claudia Krueger <ckrueger2@luc.edu>
2025-05-02 19:00:56 -0500

    Update 02table_format.R

--- 02table_format.R
+++ 02table_format.R
@@ -81,13 +81,13 @@ if (check_result != 0) {
 #system(command9)
 
 #filter SNPs
-#command10 <- paste0("awk 'NR==FNR {a[$1]; next} ($1) in a' /tmp/chrpos_allele_table.tsv /tmp/", args$pop, "_full_", args$phecode, ".tsv > /tmp/gtex_", args$phecode, ".tsv")
+command10 <- paste0("awk 'NR==FNR {a[$1]; next} ($1) in a' /tmp/chrpos_allele_table.tsv /tmp/", args$pop, "_full_", args$phecode, ".tsv > /tmp/gtex_", args$phecode, ".tsv")
 #command10 <- paste0("awk 'NR==FNR {split($1,b,\":\"); key=b[1]\":\"b[2]; a[key]; next} {key=$1\":\"$2; if(key in a) print}' /tmp/chrpos_allele_table.tsv /tmp/", args$pop, "_full_", args$phecode, ".tsv > /tmp/gtex_", args$phecode, ".tsv")
-#system(command10)
+system(command10)
 
 #save to bucket
-#command11 <- paste0("gsutil cp /tmp/gtex_", args$phecode, ".tsv ", my_bucket, "/data/", args$pop, "_gtex_", args$phecode,".tsv")
-#system(command11)
+command11 <- paste0("gsutil cp /tmp/gtex_", args$phecode, ".tsv ", my_bucket, "/data/", args$pop, "_gtex_", args$phecode,".tsv")
+system(command11)
 
 #check bucket
 check_result2 <- system(paste0("gsutil ls ", my_bucket, "/data/ | grep ", args$pop, "_gtex_", args$phecode, ".tsv"), ignore.stderr = TRUE)
