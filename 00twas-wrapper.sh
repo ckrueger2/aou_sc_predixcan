5356835f52f3037253578093b7aa7d726fb37e4b
Claudia Krueger <ckrueger2@luc.edu>
2025-05-20 13:34:18 -0500

    Update 00twas-wrapper.sh

--- 00twas-wrapper.sh
+++ 00twas-wrapper.sh
@@ -2,10 +2,10 @@
 
 #command
 usage() {
-    echo "Usage: $0 --phecode <PHECODE> --pop <POP> --ref <REF> [--databases]"
+    echo "Usage: $0 --phecode <PHECODE> --pop <POP> --ref <REF> [--gwas_h2 <h2>] [--gwas_N <n>] [--databases]"
     exit 1
 }
-
+        
 TISSUES="Adipose_Subcutaneous\nAdipose_Visceral_Omentum\nAdrenal_Gland\nArtery_Aorta\nArtery_Coronary\nArtery_Tibial\nBrain_Amygdala\nBrain_Anterior_cingulate_cortex_BA24\nBrain_Caudate_basal_ganglia\nBrain_Cerebellar_Hemisphere\nBrain_Cerebellum\nBrain_Cortex\nBrain_Frontal_Cortex_BA9\nBrain_Hippocampus\nBrain_Hypothalamus\nBrain_Nucleus_accumbens_basal_ganglia\nBrain_Putamen_basal_ganglia\nBrain_Spinal_cord_cervical_c-1\nBrain_Substantia_nigra\nBreast_Mammary_Tissue\nCells_Cultured_fibroblasts\nCells_EBV-transformed_lymphocytes\nColon_Sigmoid\nColon_Transverse\nEsophagus_Gastroesophageal_Junction\nEsophagus_Mucosa\nEsophagus_Muscularis\nHeart_Atrial_Appendage\nHeart_Left_Ventricle\nKidney_Cortex\nLiver\nLung\nMinor_Salivary_Gland\nMuscle_Skeletal\nNerve_Tibial\nOvary\nPancreas\nPituitary\nProstate\nSkin_Not_Sun_Exposed_Suprapubic\nSkin_Sun_Exposed_Lower_leg\nSmall_Intestine_Terminal_Ileum\nSpleen\nStomach\nTestis\nThyroid\nUterus\nVagina\nWhole_Blood"
 
 #command-line arguments
@@ -20,6 +20,14 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         --ref)
+            REF=$2
+            shift 2
+            ;;
+        --gwas_h2)
+            REF=$2
+            shift 2
+            ;;
+        --gwas_N)
             REF=$2
             shift 2
             ;;
