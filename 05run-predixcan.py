515fe044da322e9e40f647c413a88a2a17effbd0
Claudia Krueger <ckrueger2@luc.edu>
2025-05-20 11:25:51 -0500

    Update 05run-predixcan.py

--- 05run-predixcan.py
+++ 05run-predixcan.py
@@ -6,7 +6,7 @@ def set_args():
     parser = argparse.ArgumentParser(description="run s-predixcan")
     parser.add_argument("--phecode", help="phecode", required=True)
     parser.add_argument("--pop", help="population", required=True)
-    parser.add_argument("-r", "--reference", help="eqtl model and matrix to use as reference", required=True)
+    parser.add_argument("--ref", help="eqtl model and matrix to use as ref", required=True)
     return parser
 
 def main():
@@ -14,25 +14,26 @@ def main():
     args = parser.parse_args(sys.argv[1:])
 
     #get file from bucket
-    bucket = os.environ.get("WORKSPACE_BUCKET")
+    bucket = os.getenv('WORKSPACE_BUCKET')
     filename = args.pop + "_formatted_gtex_" + args.phecode + ".tsv"
-    get_file = "gsutil cp " + bucket + "/data/" + filename + " ."
+    get_command = "gsutil cp " + bucket + "/data/" + filename +  " /tmp/"
+    os.system(get_command)
     
-    os.system(get_file)
-    output = f"{args.pop}_predixcan_output_{args.phecode}.csv"
+    output = f"/home/jupyter/{args.pop}_predixcan_output_{args.phecode}.csv"
 
-    #define python path
+    #define python and metaxcan paths
     python_path = sys.executable
+    metaxcan_dir = "/home/jupyter/MetaXcan"
     
-    os.system(f"{python_path} MetaXcan/software/SPrediXcan.py \
-    --gwas_file /home/jupyter/GWAS-TWAS-in-All-of-Us-Cloud/{filename} \
+    os.system(f"{python_path} {metaxcan_dir}/software/SPrediXcan.py \
+    --gwas_file /tmp/{filename} \
     --snp_column SNP \
     --effect_allele_column ALT \
     --non_effect_allele_column REF \
     --beta_column BETA \
     --se_column SE \
-    --model_db_path eqtl/mashr/mashr_{args.reference}.db \
-    --covariance eqtl/mashr/mashr_{args.reference}.txt.gz \
+    --model_db_path eqtl/mashr/mashr_{args.ref}.db \
+    --covariance eqtl/mashr/mashr_{args.ref}.txt.gz \
     --keep_non_rsid \
     --additional_output \
     --model_db_snp_key varID \
