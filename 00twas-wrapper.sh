212234271eb46235286077490454bf58c0c8bd5b
Claudia Krueger <ckrueger2@luc.edu>
2025-05-20 14:29:05 -0500

    Update 00twas-wrapper.sh

--- 00twas-wrapper.sh
+++ 00twas-wrapper.sh
@@ -67,11 +67,6 @@ conda create -n imlabtools python=3.8 numpy=1.19 pandas=1.1 scipy -y
 #activate imlabtools
 if conda activate imlabtools; then
     echo "Successfully activated imlabtools environment"
-    #install hail within the imlabtools environment
-    echo "Installing Hail"
-    if pip install hail --quiet --no-warn-script-location; then
-        echo "Successfully installed/loaded Hail package"
-    fi
 fi
 
 #patch metaxcan code if needed
