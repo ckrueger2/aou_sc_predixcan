0aa7aa76d30bd1a32266e38fb3b494a720e29005
Claudia Krueger <ckrueger2@luc.edu>
2025-05-20 14:21:25 -0500

    Update 00twas-wrapper.sh

--- 00twas-wrapper.sh
+++ 00twas-wrapper.sh
@@ -70,7 +70,7 @@ if conda activate imlabtools; then
     #install hail within the imlabtools environment
     echo "Installing Hail"
     if pip install hail --quiet --no-warn-script-location; then
-        pip install numpy==1.19.* --force-reinstall --quiet --no-warn-script-location
+        pip install numpy==1.19.* --force-reinstall --quiet --no-warn-script-location 2>/dev/null
         echo "Successfully installed/loaded Hail package"
     fi
 fi
