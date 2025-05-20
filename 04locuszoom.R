3385e007b86b4b2c761b5a11180f435b28d0094f
Claudia Krueger <ckrueger2@luc.edu>
2025-05-20 09:03:56 -0500

    Update 04locuszoom.R

--- 04locuszoom.R
+++ 04locuszoom.R
@@ -43,17 +43,10 @@ if (!is.null(args$rsid)) {
   #creating locus object with user provided SNP
   loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = args$rsid, flank = 1e5)
 } else { 
-  #rsid is not provided, so we default to most signif. SNP
+  #rsid is not provided, so we default to most significant SNP
   data$Pvalue <- as.numeric(as.character(data$Pvalue)) #ensure p-values are numeric
-  if(any(is.na(data$Pvalue))) {
-    cat("Warning: NA values found in Pvalue column, removing NA rows from analysis\n")
-    signif_rsid <- which.min(data$Pvalue[!is.na(data$Pvalue)])
-    cat(signif_rsid, "\n")
-  } else {
-    signif_rsid <- data$rsID[which.min(data$Pvalue)]
-    cat(signif_rsid, "\n")
-  }
-  # creating locus object with top hit SNP
+  min_idx <- which.min(data$Pvalue) #find minimum index
+  signif_rsid <- data$rsID[min_idx] #find snp at minimum index
   cat("rsID not provided; running locuszoom on lowest p-value SNP\n")
   loc <- locus(data = data, ens_db = "EnsDb.Hsapiens.v86", index_snp = signif_rsid, flank = 1e5)
 }
