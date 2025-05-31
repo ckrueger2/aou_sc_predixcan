17aae7c4950ecbaa105a662256bd68738f69e603
Claudia Krueger <ckrueger2@luc.edu>
2025-05-31 15:18:40 -0500

    Update 08coloc.R

--- 08coloc.R
+++ 08coloc.R
@@ -1,20 +1,33 @@
+#if needed, install packages
+if (!requireNamespace("data.table", quietly = TRUE)) install.packages("data.table")
+if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
 if (!requireNamespace("coloc", quietly = TRUE)) install.packages("coloc")
+if (!requireNamespace("argparse", quietly = TRUE)) install.packages("argparse")
 
-library(dplyr)
+#load packages
 library(data.table)
+library(dplyr)
 library(coloc)
+library(argparse)
+
+#set up argparse
+parser <- ArgumentParser()
+parser$add_argument("--phecode", help="all of us phenotype ID")
+parser$add_argument("--pop", help="all of us population ID")
+
+args <- parser$parse_args()
 
 #find bucket
 my_bucket <- Sys.getenv('WORKSPACE_BUCKET')
 
 #read in MESA pQTL table
-name_of_pqtl_file <- "META_mesa_pqtls.txt"
+name_of_pqtl_file <- paste0(args$pop, "_mesa_pqtls.txt")
 pqtl_command <- paste0("gsutil cp ", my_bucket, "/data/", name_of_pqtl_file, " .")
 system(pqtl_command, intern=TRUE)
 qtl_data <- fread(name_of_pqtl_file, header=TRUE)
 
 #read in AoU GWAS table
-name_of_gwas_file <- "META_formatted_mesa_CV_404.tsv"
+name_of_gwas_file <- paste0(args$pop, "_formatted_mesa_, ", args$phecode, ".tsv"
 gwas_command <- paste0("gsutil cp ", my_bucket, "/data/", name_of_gwas_file, " .")
 system(gwas_command, intern=TRUE)
 gwas_data <- fread(name_of_gwas_file, header=TRUE)
@@ -67,7 +80,7 @@ for (phenotype in unique_phenotypes) {
       varbeta = merged_data$slope_se^2,
       snp = merged_data$ID,
       type = "quant",
-      N = 2953, #META: 2953, EUR: 1270
+      N = if (args$pop == "EUR") 1270 else 2953, #META: 2953, EUR: 1270
       MAF = merged_data$af
     )
     
@@ -76,7 +89,7 @@ for (phenotype in unique_phenotypes) {
       varbeta = merged_data$SE^2,
       snp = merged_data$ID,
       type = "quant",
-      N = 276112,  #META: 276112, EUR: 111887
+      N = if (args$pop == "EUR") 111887 else 276112, #META: 276112, EUR: 111887
       sdY = 1
     )
     
