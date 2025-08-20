ce27103c7ea37c002ed8db434461442647d5723b
Claudia Krueger <ckrueger2@luc.edu>
2025-08-20 18:58:02 -0500

    Update 02table_format.R

--- 02table_format.R
+++ 02table_format.R
@@ -97,80 +97,31 @@ sc_table$CHR <- gsub("Y", "24", sc_table$CHR)
 
 #FINAL FORMATTING
 #format chromosomes
-filtered_merged_table$CHR <- gsub("chr", "", filtered_merged_table$CHR)
 sc_table$CHR <- gsub("chr", "", sc_table$CHR)
 sc_table$CHR <- gsub("X", "23", sc_table$CHR)
 sc_table$CHR <- gsub("Y", "24", sc_table$CHR)
-#mesa_table$CHR <- gsub("chr", "", mesa_table$CHR)
-#mesa_table$CHR <- gsub("X", "23", mesa_table$CHR)
-#mesa_table$CHR <- gsub("Y", "24", mesa_table$CHR)
-
-#make numeric
-filtered_merged_table$CHR <- as.numeric(filtered_merged_table$CHR)
-sc_table$CHR <- as.numeric(sc_table$CHR)
-#mesa_table$CHR <- as.numeric(mesa_table$CHR)
-filtered_merged_table$POS <- as.numeric(filtered_merged_table$POS)
-
-#read in mesa rsIDs
-#mesa_data <- fread("/tmp/", args$pop, "_mesa_snps.txt", header=TRUE, sep=",")
-
-#this merge is finicky, re-format everything
-#mesa_data$chr <- paste0("chr", gsub("^chr", "", mesa_data$chr))
-#mesa_table$CHR <- paste0("chr", gsub("^chr", "", mesa_table$CHR))
-#mesa_data$pos <- as.integer(as.character(mesa_data$pos))
-#mesa_table$POS <- as.integer(as.character(mesa_table$POS))
-#mesa_data$chr <- trimws(mesa_data$chr)
-#mesa_data$rsid <- trimws(mesa_data$rsid)
-#mesa_table$CHR <- trimws(mesa_table$CHR)
-
-#create unique key for matching 
-#mesa_data$merge_key <- paste(mesa_data$chr, mesa_data$pos, sep="_")
-#mesa_table$merge_key <- paste(mesa_table$CHR, mesa_table$POS, sep="_")
-
-#merge mesa table and data
-#merged_mesa_table <- merge(mesa_table, mesa_data[, c("merge_key", "rsid")], by = "merge_key", all.x = TRUE)
-
-#clean up the merge key column
-#merged_mesa_table$merge_key <- NULL
 
 #sort by chr, pos
-filtered_merged_table <- filtered_merged_table %>%
-  arrange(CHR, POS)
 sc_table <- sc_table %>%
   arrange(CHR, POS)
-#merged_mesa_table <- merged_mesa_table %>%
-#  arrange(CHR, POS)
 
 #rename header
 sc_table$"#CHROM" <- sc_table$CHR
 sc_table$CHR <- NULL
-#merged_mesa_table$"#CHROM" <- merged_mesa_table$CHR
-#merged_mesa_table$CHR <- NULL
 
 #select columns
 sc_table <- sc_table %>%
   select(locus, alleles, ID, REF, ALT, "#CHROM", BETA, SE, Pvalue, SNP)
-#merged_mesa_table <- merged_mesa_table %>%
-#  select(locus, alleles, ID, REF, ALT, "#CHROM", BETA, SE, Pvalue, SNP, rsid, ID)
 
 #edit X chromosome SNP file for sc table
 sc_table$SNP <- gsub("^chrX_", "X_", sc_table$SNP)
 
-#edit locus column for pvalue filtered table
-filtered_merged_table$locus <- gsub("^chrX:", "X:", filtered_merged_table$locus)
-
-#check tables
-cat("Final pvalue filtered table:\n")
-head(filtered_merged_table)
-
-cat("Final sc filtered table:\n")
+#check table
+cat("Final single cell filtered table:\n")
 head(sc_table)
 
-#cat("Final MESA filtered table:\n")
-#head(merged_mesa_table)
-
 #write sc table
-sc_destination_filename <- paste0(args$pop, "_formatted_sc_", args$phecode,".tsv")
+sc_destination_filename <- paste0(args$pop, "_formatted_sc_", args$cell_type, "_", args$phecode,".tsv")
 
 #store the dataframe in current workspace
 write.table(sc_table, sc_destination_filename, col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
